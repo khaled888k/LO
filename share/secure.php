c5bc5f8c30e875d9dbd524f1d1104db8d19da821
sko0otr <55184243+sko0otr@users.noreply.github.com>
2019-11-24 09:56:17 +0300

    Update secure.php

--- share/secure.php
+++ share/secure.php
@@ -13,13 +13,13 @@ $user = strtolower($_POST["user"]);
 $pass = crypt($_POST['pass'],	$user);
 include("conn.php");
         
-$result = mysqli_query($conn, "SELECT pass, id FROM users WHERE email='$user'") or die(mysqli_error($conn));		
+$result = mysqli_query($conn, "SELECT pass, id FROM users WHERE u_id_number='$user'") or die(mysqli_error($conn));		
 $row = mysqli_fetch_assoc($result);
 		
 		
 		if($row['pass'] == $pass){
 			$_SESSION["status"] = "Logged";
-            $_SESSION["us_id"] = $row['id'];
+            $_SESSION["u_id"] = $row['id'];
 			$allow = 1;
 		}else{$err = "<div style='color:red'>عفوا، اسم المستخدم و كلمة المرور خطأ</div>";}
 	}
