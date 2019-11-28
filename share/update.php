21bb6b475fac7b3a7c3d28120919c5f5ccd87426
khaled888k <50434716+khaled888k@users.noreply.github.com>
2019-11-28 10:24:54 +0300

    Update update.php

--- share/update.php
+++ share/update.php
@@ -1,71 +1,116 @@
+<?php include("header.php");?>
 <?php include("conn.php");?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title></title>
     <meta charset="UTF-8">
+
+    <style>
+        tr{
+            width:80%;
+        }
+    </style>
 </head>
 <body dir="rtl">
-
+<div class="container-fluide" >
     <?php
     
-    if(isset($_POST["u_fullname"])){
+    if(isset($_POST['update'])){
         
-        mysqli_query($conn, "UPDATE users SET u_fullname='$_POST[name]', u_email='$_POST[cmail]', u_neighborhood='$_POST[neighborhood]', u_birthday='$_POST[birthday]', u_mobile='$_POST[mobile]', u_education='$_POST[education]' WHERE u_id='$_POST[id]'");
+        $q = "UPDATE users SET u_fullname = '".$_POST['fullname']."', u_email= '".$_POST['cmail']."' , u_neighborhood= '".$_POST['neighborhood']."', u_birthday= '".$_POST['birthday']."', u_mobile= '".$_POST['mobile']."', u_education='".$_POST['education']."' WHERE u_id= '".$_POST['id']."'";
+        mysqli_query($conn, $q);
         echo mysqli_error($conn);
     }
     ?>
+
      <?php
 
     if(isset($_GET["id"])){
        
-       $result = mysqli_query($conn, "SELECT * FROM users WHERE u_id=$_GET[id]");
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE u_id= $_GET[id]");
     $row = mysqli_fetch_array($result);
-    }
-       ?>
-        
-        
-    <form method="post">
-     اسم الطالبه	 :
-        <input type="hidden" name="id" value="<?= $row["u_id"]?>" >
-        <br>
-        <input type="text" name="fullname" required value="<?= $row["u_fullname"]?>"><br>
-                <hr>
-    الايميل :
-         <br>
-        <input name="cmail" type="email" value="<?= $row["u_email"]?>">
-        <hr>
-         
-        الحي
-         <br>
-        <input name="neighborhood	" type="text" value="<?= $row["u_neighborhood"]?>">
-                <hr>
-          
-       تاريخ الميلاد
-         <br>
-        <input name="birthday" type="text" value="<?= $row["u_birthday"]?>">
-                <hr>
-          
-       الرقم الجوال
-         <br>
-        <input name="mobile" type="text" value="<?= $row["u_mobile"]?>">
-                <hr>
-         
-         الموهل العلمي
-         <br>
-        <input name="education" type="text" value="<?= $row["u_education"]?>">
-                <hr>
-         <br>
-        
-        <input type="submit" value="تعديل">
-        
+    echo'
+    <br><br><br><br>
+    <form method="post" role="form">
     
+
+        <input class="form-control" type="hidden" name="id" value="'.$row["u_id"].'" >
+
+        
+        <table style="margin-right:10px;">
+            <tr>
+            <td> <label for="fullname" style="text-align:right;">اسم الطالبه	 :</label></td><td><input  class="form-control" type="text" name="fullname" required value="'.$row["u_fullname"].'"><br></td>
+            </tr>       
+            
+                
+            <tr>
+            <td> <label for="cmail" style="text-align:right;">الايميل :</label ></td><td><input class="form-control" name="cmail" type="email" value="'.$row["u_email"].'"></td>
+            </tr>
+            
+
+            <tr>
+            <td> <label for="neighborhood" style="text-align:right;">الحي :</label></td><td><input  class="form-control"name="neighborhood" type="text" value="'.$row["u_neighborhood"].'"></td>
+            </tr>
+                
+                
+            <tr>
+            <td> <label for="birthday">تاريخ الميلاد :</label></td><td> <input  name="birthday" class="form-control" type="text" value="'.$row["u_birthday"].'"></td>
+            </tr> 
+        
+            
+        
+        
+            <tr>
+            <td><label for="mobile">الرقم الجوال :</label><br></td><td> <input  class="form-control" name="mobile" type="text" value="'.$row["u_mobile"].'"></td>
+            </tr> 
+
+            <tr>
+            <td><label for="education"> الموهل العلمي :</label><br></td><td> <input  class="form-control" name="education" type="text" value="'.$row["u_education"].'"></td>
+            </tr> 
+            
+            
+            
+            <tr>
+             
+            ';
+             $result = mysqli_query($conn, "select courses.co_name from courses , users , rigster where users.u_id = rigster.u_id and courses.co_id = rigster.c_id and users.u_id = ".$row["u_id"] ."");
+             while($row2 = mysqli_fetch_array($result)){
+             echo' 
+                 
+                 <td><label for="education"> الدورات :</label><br></td><td> <input  class="form-control" name="education" type="text" value="'.$row2["co_name"].'"></td>
+            ';
+             }
+           echo '
+                    
+            </tr>
+            
+            
+           
+
+    </table>
+    <div style="background-color:#eee; padding:10px; margin-top:150px;">
+     <center>
+        <input class="btn btn-md btn-outline-success" type="submit" class="form-control" name="update" value="تعديل">
+         
+        <a class="btn btn-md btn-outline-success" href="del.php?id='.$row["u_id"].'" style="color:red" onclick="return confirm(' .$row["u_fullname"].')">حذف</a>
+        
+        
+         
+
+         
+    </center>
+    </div>
     </form>
-     <input type="submit" value="تعديل">
-     <hr>
-    <a href="del.php?id=<?= $row["u_id"]?>" style="color:red" onclick="return confirm('هل انت متأكد من حذف  <?= $row["u_fullname"]?>؟ ')">حذف</a>
+
+    '; 
+        }
+
+    ?>
+
      
     
-    
+    </div>
 </body>
 </html>
