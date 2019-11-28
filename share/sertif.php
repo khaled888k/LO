3b710e6c42d4eb09cb8216b517df6aabf11316c7
khaled888k <50434716+khaled888k@users.noreply.github.com>
2019-11-28 10:22:11 +0300

    Update sertif.php

--- share/sertif.php
+++ share/sertif.php
@@ -1,4 +1,4 @@
-<?php include("header.php");?>
+
 <?php include("conn.php");?>
 
 <!DOCTYPE html>
@@ -8,7 +8,8 @@
 
     <meta charset="UTF-8">
 </head>
-<body>
+<body class="rtl">
+    <div class="container"> 
     <br>
     <br>
     <br>
@@ -24,24 +25,34 @@
 
     if(isset($_GET["id"])){
        
-       $result = mysqli_query($conn, "SELECT * FROM users WHERE u_id=$_GET[id]");
-    $row = mysqli_fetch_array($result);
-    }
-       ?>
-<div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
-<div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
+       $q = "SELECT * FROM users WHERE u_id = ".$_GET["id"]."";
+       $result = mysqli_query($conn, $q);
+       $row = mysqli_fetch_array($result);
+    
+       echo'
+       <div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
+       <div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
        <span style="font-size:50px;ادارة المركز font-weight:bold">شهادة  دورة</span>
        <br><br>
        <span style="font-size:25px"><i>تشهد ادارة الحي المتعلم بأن الطالبة</i></span>
        <br><br>
-       <span style="font-size:30px"><b> اسم الطالبة:</b><?=$row['u_fullname']?></span><br/><br/>
-       <span style="font-size:25px"><i>قد حضرت دورة بعنوان</i></span> 
-       <span style="font-size:30px">عنوان الدورة</span> <br/><br/>
+       <span style="font-size:30px ;"><b style="color:red;">'.$row['u_fullname'].'</b> اسم الطالبة:</span><br/><br/>
+       <span style="font-size:25px"><i>قد حضرت دورة/دورات بعنوان</i>';
+            $result = mysqli_query($conn, "select courses.co_name from courses , users , rigster where users.u_id = rigster.u_id and courses.co_id = rigster.c_id and users.u_id = ".$row["u_id"] ."");
+            while($row = mysqli_fetch_array($result)){
+            echo' 
+                <li style="color:red; text-decoration:none;">'.$row["co_name"].'</li>
+            ';
+            }
+        echo '
+       </span> 
        <span style="font-size:20px">وبناء على ذلك منحت هذه الشهاده </span>
-        <span style="font-size:25px"><i>الختم</i></span><br> <br>  <br> <br>  
+       <span style="font-size:25px"><i>الختم</i></span><br> <br>  <br> <br>  
      
       <span style="font-size:30px">ادارة الحي المتعلم</span>
-</div>
+      ';
+    }
+      ?>
 </div>
 </body>
 </html>
