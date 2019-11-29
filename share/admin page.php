e73dc25f5e8acdaffb3a3f199f55ff2c077f5d35
abdulrahmanalsiadon <55184184+abdulrahmanalsiadon@users.noreply.github.com>
2019-11-29 23:37:30 +0300

    Add files via upload

--- share/admin page.php
+++ share/admin page.php
@@ -41,145 +41,36 @@
     
   <div class="album py-5 bg-light">
     <div class="container">
-
       <div class="row" dir="rtl">
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/makeup.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">فن المكياج</text>
-            <div class="card-body">
-           
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                  <button  type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"> <a href="report.php">تقارير شامله</a></button>
-                </div>
-               
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/cooking.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">مهارات الطبخ</text>
-            <div class="card-body">
-             
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                   <button type="button" class="btn btn-sm btn-outline-secondary" >تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-              
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/computer.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">مهارات الحاسب الالي</text>
-            <div class="card-body">
-             
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                 <button type="button"  class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-        
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/n7t.jpeg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">الرسم والنحت على الخشب</text>
-            <div class="card-body">
-            
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-              
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/cor.jpg""xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">الكوريشه</text>
-            <div class="card-body">
-              
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                   <button type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-                
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/deco.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">الديكوباج</text>
-            <div class="card-body">
-          
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                   <button type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-               
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/writing.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">تقويه القراءة وتحسين الخط</text>
-            <div class="card-body">
-          
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-               <button type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
+
+    <?php
+      $q = "select * from courses";
+      $result = mysqli_query( $conn,$q );
+
+      while($row = mysqli_fetch_array($result)){
+        echo ' 
+              <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/'.$row["course_image"].'" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">'.$row["co_name"].'</text>
+                  <div class="card-body">
+                    <div class="navbar-nav mr-auto">
+                      <div class="btn-group">
+                        <a   class="btn btn-sm btn-outline-secondary" href="attendence.php?course_id='.$row['co_id'].'">تحضير</a>
+                        <a   class="btn btn-sm btn-outline-secondary" href="report.php?course_id='.$row['co_id'].'"> تقارير شامله</a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              
               </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/tooking.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">فن التعامل مع الاخرين</text>
-            <div class="card-body">
-             
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                   <button type="button" class="btn btn-sm btn-outline-secondary" >تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-             
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="img/tiers.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">تدوير الخامات</text>
-            <div class="card-body">
-             
-              <div class="navbar-nav mr-auto">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">تحضير</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary"><a href="report.php">تقارير شامله</a></button>
-                </div>
-        
-              </div>
-            </div>
-          </div>
-        </div>
+        ';
+      }
+
+
+    ?>
+
+
       </div>
     </div>
   </div>
--- share/attendence.php
+++ share/attendence.php
@@ -2,9 +2,35 @@
 <!DOCTYPE html>
 <html>
 
+<?php
+if(isset($_GET["update_attendanse"])){
+    echo 'alert("called")';
+       if($_POST["state"] == true){
+            $result = mysqli_query($conn, "update rigster set is_present = false WHERE u_id= $_POST[u_id]");
+            echo 'alert("falsed")';
+       }else{
+            $result = mysqli_query($conn, "update rigster set is_present = true WHERE u_id= $_POST[u_id]");
+            echo 'alert("trued")';
+       }
+       header("location:attendence.php");
+}
+?>
+
 <head>
    <title> الحي المتعلم </title>
  
+    <script type="text/javascript">
+       function get_statues(us_id){
+        <?php
+            $user_id = 1;
+            echo'alert("'.$user_id.'")';
+            $q = "SELECT u_fullname FROM users where u_id = '$user_id'";
+            $result = mysqli_query( $conn,$q );
+            $row = mysqli_fetch_array($result);
+            echo'alert("'.$row["u_fullname"].'")';
+        ?>
+       }
+    </script>
     <style>
         table.GeneratedTable {
   width: 25%;
@@ -44,13 +70,17 @@ table.GeneratedTable thead {
         </style>
             
      <img class="bd-placeholder-img card-img-top" width="100%" height="225" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail" src="img/download.png">
-
+<div class="container-table100">
+ <div class="wrap-table100">
+  <div class="table100" >
     <table class="GeneratedTable">
         <thead>
 
             <tr>
+                <th>رقم الطالبه</th>
                 <th>اسم الطالبه</th>
-                <th>التحضير</th>
+                <th>حاضر؟</th>
+                <th>تحضير</th>
 
             </tr>
         </thead>
@@ -58,170 +88,63 @@ table.GeneratedTable thead {
 
             <div class="group">
                 <label for="user" class="label"> اسم المدربة: </label>
-                <input id="user" type="text" class="input">
+                <?php 
+                    $q = "SELECT lecturer_name FROM courses where co_id = ".$_GET["course_id"]."";
+                    $result = mysqli_query( $conn,$q );
+                    $row = mysqli_fetch_array($result);
+                    echo '<input id="user" type="text" class="input" value = "'.$row["lecturer_name"].'">';  
+                ?>
             </div>
-            <br>
             <div class="group">
                 <label for="user" class="label"> اسم الدوره: </label>
-                <input id="user" type="text" class="input">
+                <?php 
+                    $q = "SELECT co_name FROM courses where co_id = ".$_GET["course_id"]."";
+                    $result = mysqli_query( $conn,$q );
+                    $row = mysqli_fetch_array($result);
+                    echo '<input id="user" type="text" class="input" value = "'.$row["co_name"].'">';  
+                ?>              
             </div>
+
             <br>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
+            <?php
+                
+                $q = "SELECT users.u_id, users.u_fullname, rigster.is_present FROM users ,courses , rigster  where  courses.co_id = rigster.c_id and  users.u_id = rigster.u_id and rigster.c_id = ".$_GET["course_id"]."";
+                $result = mysqli_query( $conn,$q );
+                while($row = mysqli_fetch_array($result)){
+                    echo '  
+                        <tr>
+                            <td >'.$row["u_id"].'</td>
+                            <td >'.$row["u_fullname"].'</td>
+                            <td>
+                                <!-- Material checked -->
+                                <div class="form-check" width="300px">    
+                                    <label class="form-check-label" for="is_presentCheckBox">حاضر</label>   
+                                    <input type="checkbox" class="form-check-input" id="is_presentCheckBox" onClick="get_statues('.$row["u_id"].')"'; if($row["is_present"]== 1) echo 'checked'; echo '>
+                                </div>
+                            </td>
+                            <td>
+                                    <a name="update_attendanse" href="change_attendanse.php?u_id='.$row['u_id'].'&state='.$row['is_present'].'&course_id='.$_GET['course_id'].'" class="btn btn-md btn-outline-success" >تغيير</a>
+                            </td>     
+                        </tr>                 
+                    ';
+                }
+            ?>
 
 
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-
-
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-
-
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-
-
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
-
-
-            <tr>
-                <td>wssssssssssssssssssssssssssssssssssss</td>
-                <td>
-                    <!-- Material checked -->
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-                        <label class="form-check-label" for="materialChecked2">حاضر</label>
-                    </div>
-                </td>
-
-
-            </tr>
         </tbody>
-    </table>
+      </table>
+     </div>
     </div>
+    </div>
+    </div>
+    <br/>
+    <div class="container" style="border:1px solid #ddd; padding:5px 0px 5px 0px; background-color:#eee;">
+                <center>
+                    <a class="btn btn-md btn-outline-success" href="#">save</a>
+                </center>
+    </div>
+
+    <br/><br/><br/>
 </body>
 
 </html>
