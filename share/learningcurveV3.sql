6506ccf26f1308a5f4e7e1d017479c362d8bbf14
sko0otr <55184243+sko0otr@users.noreply.github.com>
2019-12-14 22:52:47 +0300

    تعليقات على قاعة البيانات

--- share/learningcurveV3.sql
+++ share/learningcurveV3.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2019 at 01:58 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.0.23
+-- Generation Time: Dec 14, 2019 at 08:50 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `learning_curve`
+-- Database: `learningcurve`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `center` (
-  `ce_id` int(11) NOT NULL,
-  `ce_name` varchar(255) NOT NULL,
-  `ce_location` varchar(255) NOT NULL,
+  `ce_id` int(11) NOT NULL COMMENT 'ترقيم تلفاىي',
+  `ce_name` varchar(255) NOT NULL COMMENT 'اسم المركز',
+  `ce_location` varchar(255) NOT NULL COMMENT 'مكان المركز',
   `ce_manger` int(255) NOT NULL COMMENT 'u_id (Admin)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,7 +42,8 @@ CREATE TABLE `center` (
 INSERT INTO `center` (`ce_id`, `ce_name`, `ce_location`, `ce_manger`) VALUES
 (1, 'الربعه', 'الربوه', 8),
 (2, 'الاول', 'الشماس', 0),
-(3, 'الجبيل', 'مكه', 0);
+(3, 'الجبيل', 'مكه', 0),
+(4, '57', 'الرفيعه', 0);
 
 -- --------------------------------------------------------
 
@@ -51,11 +52,11 @@ INSERT INTO `center` (`ce_id`, `ce_name`, `ce_location`, `ce_manger`) VALUES
 --
 
 CREATE TABLE `classes` (
-  `c_id` int(11) NOT NULL,
-  `ce_id` int(11) NOT NULL,
-  `co_id` int(11) NOT NULL,
-  `c_start` date NOT NULL,
-  `c_end` date NOT NULL
+  `c_id` int(11) NOT NULL COMMENT 'ترقيم تلفاىي',
+  `ce_id` int(11) NOT NULL COMMENT 'رقم المركز',
+  `co_id` int(11) NOT NULL COMMENT 'رقم الدوره',
+  `c_start` date NOT NULL COMMENT 'بداية الدوره',
+  `c_end` date NOT NULL COMMENT 'نهايه الدوره'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -74,12 +75,12 @@ INSERT INTO `classes` (`c_id`, `ce_id`, `co_id`, `c_start`, `c_end`) VALUES
 --
 
 CREATE TABLE `courses` (
-  `co_id` int(11) NOT NULL,
-  `co_name` varchar(255) NOT NULL,
-  `co_details` varchar(255) NOT NULL,
-  `co_hours` varchar(120) NOT NULL,
-  `course_image` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `lecturer_name` varchar(50) CHARACTER SET utf8 NOT NULL
+  `co_id` int(11) NOT NULL COMMENT 'ترقيم تلفاىي',
+  `co_name` varchar(255) NOT NULL COMMENT 'اسم الدوره',
+  `co_details` varchar(255) NOT NULL COMMENT 'تفاصيل الدوره',
+  `co_hours` varchar(120) NOT NULL COMMENT 'ساعات الدوره',
+  `course_image` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT 'صورة الدورة',
+  `lecturer_name` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT 'معلمة الدوره'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -113,9 +114,9 @@ CREATE TABLE `pwdreset` (
 
 CREATE TABLE `rigster` (
   `r_id` int(11) NOT NULL COMMENT 'رقم تقائي التسجيل',
-  `u_id` int(11) NOT NULL,
-  `c_id` int(11) NOT NULL,
-  `is_present` tinyint(1) NOT NULL
+  `u_id` int(11) NOT NULL COMMENT 'رقم المستخدم',
+  `c_id` int(11) NOT NULL COMMENT 'رقم الكلاس',
+  `is_present` tinyint(1) NOT NULL COMMENT 'تحضير الطالبات'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -135,16 +136,16 @@ INSERT INTO `rigster` (`r_id`, `u_id`, `c_id`, `is_present`) VALUES
 --
 
 CREATE TABLE `users` (
-  `u_id` int(11) NOT NULL,
-  `u_id_number` varchar(10) NOT NULL,
-  `u_fullname` varchar(255) NOT NULL,
-  `u_email` varchar(255) NOT NULL,
-  `u_neighborhood` varchar(255) NOT NULL,
-  `u_birthday` date NOT NULL,
-  `u_mobile` varchar(200) NOT NULL,
-  `u_education` varchar(255) NOT NULL,
-  `u_type` varchar(200) NOT NULL DEFAULT 'Student',
-  `pass` varchar(200) NOT NULL
+  `u_id` int(11) NOT NULL COMMENT 'ترقيم تلفاىي',
+  `u_id_number` varchar(10) NOT NULL COMMENT 'رقم السجل المدني',
+  `u_fullname` varchar(255) NOT NULL COMMENT 'اسم المستخدم كامل',
+  `u_email` varchar(255) NOT NULL COMMENT 'ايميل المستخدم',
+  `u_neighborhood` varchar(255) NOT NULL COMMENT 'حي السكن',
+  `u_birthday` date NOT NULL COMMENT 'يوم الميلاد',
+  `u_mobile` varchar(200) NOT NULL COMMENT 'رقم الجوال',
+  `u_education` varchar(255) NOT NULL COMMENT 'المستوى التعليمي',
+  `u_type` varchar(200) NOT NULL DEFAULT 'Student' COMMENT 'نوع المستخدم',
+  `pass` varchar(200) NOT NULL COMMENT 'كلمة مرور المستخدم'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -153,7 +154,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`u_id`, `u_id_number`, `u_fullname`, `u_email`, `u_neighborhood`, `u_birthday`, `u_mobile`, `u_education`, `u_type`, `pass`) VALUES
 (8, '', 'ali', 'ali@example.com', '', '0000-00-00', '', '', 'Admin', '$2y$10$TuIa7LNkGCidZUdhh6UTEemFvlKdu.iadp5JDqRWJIVhL3ZEsPDkK'),
-(9, '', 'nm', 'nm@yahoo.com', '', '0000-00-00', '', '', 'Student', '$2y$10$LIELTuGkdql4pVZlPXRXneEItZySmr2nSXhz9YlIC/pqxa08A3N2G');
+(9, '', 'nm', 'nm@yahoo.com', '', '0000-00-00', '', '', 'Student', '$2y$10$LIELTuGkdql4pVZlPXRXneEItZySmr2nSXhz9YlIC/pqxa08A3N2G'),
+(10, '', 'xa', 'mmmmm.0555555@gmail.com', '', '0000-00-00', '', '', 'Student', '$2y$10$59/TdOWQMDThXRfcXC67n.JeptPmWrrXweGVwGRvtrKrSN5ZBZz9y'),
+(11, '', 'za', 's@s.com', '', '0000-00-00', '', '', 'Student', '$2y$10$Hcy9KgG/7z.Va52iHgca0uSdmSr3FDFYT1yrwTnXNe4OEaOIDa42O'),
+(12, '', 're', 's@s.com', '', '0000-00-00', '', '', 'Student', '$2y$10$dWwppPxfchH.eLz1o8rk4.uGJcbdrxvxqNeE5omcV3kXGr.Jx2Joq');
 
 --
 -- Indexes for dumped tables
@@ -203,19 +207,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `center`
 --
 ALTER TABLE `center`
-  MODIFY `ce_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ce_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ترقيم تلفاىي', AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ترقيم تلفاىي', AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `co_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `co_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ترقيم تلفاىي', AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pwdreset`
@@ -233,7 +237,7 @@ ALTER TABLE `rigster`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ترقيم تلفاىي', AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
